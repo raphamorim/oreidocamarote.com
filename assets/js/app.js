82760eb7998db30802d137023705755ca00fdeea
Raphael Amorim <rapha850@gmail.com>
2015-06-06 19:58:23 -0300

    dando vida ao camarote - parte1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,17 @@
-function trazOPlaceholderQuePisca(quantos) {
-  var lorem = new Lorem;
-  lorem.type = Lorem.TEXT;
-  lorem.query = quantos + 'p';
-  lorem.createLorem(document.getElementById('lorem'));
-}
+(function(){
+	var gera = document.querySelector('#gera'),
+		geraBtn = document.querySelector('#gera-btn');
 
+	function trazOPlaceholderQuePisca(quantos) {
+  		var lorem = new Lorem;
+  		lorem.type = Lorem.TEXT;
+  		lorem.query = quantos + 'p';
+  		lorem.createLorem(document.getElementById('lorem'));
+	}
 
-document.getElementById('gera').addEventListener("click", function() {
-  trazOPlaceholderQuePisca(2);
-});
+	gera.addEventListener("click", function() {  
+  		gera.style.marginTop = '50px';	
+  		geraBtn.innerHTML = 'Agregue mais valor ao seu código';
+  		trazOPlaceholderQuePisca(1);
+	})
+})()
