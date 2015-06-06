0357eee7b7a61966673673620c8055e76983c86f
Andre Alves Garzia <contato@andregarzia.com>
2015-06-06 18:46:22 -0300

    agregando valor

---
+++ agrega.js
@@ -0,0 +1,15 @@
+cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
+  alert('Agora você tem champagne infinito!!!!!');
+});
+
+cheet('c a m a r o t e', function() {
+  document.body.className = "blink";
+});
+
+cheet('b a l a d a', function() {
+  document.body.className = "balada";
+});
+
+cheet('i d d q d', function () {
+  alert('god mode enabled');
+});
